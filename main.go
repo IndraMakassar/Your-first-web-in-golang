625a691487cb05df550957cec43d99fb492b19ea
Antonius Indra Dharma Prasetya <anindhpra@gmail.com>
2023-11-14 16:05:16 +0800

    selesai

---
+++ main.go
@@ -0,0 +1,70 @@
+package main
+
+import "fmt"
+import "errors"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	router := gin.Default()
+
+	var library []Book
+
+	router.LoadHTMLGlob("templates/*")
+
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.tmpl", gin.H{"library": library})
+	})
+
+	router.POST("/add", func(c *gin.Context) {
+		book, err := createBook()
+		if err != nil {
+			c.HTML(http.StatusBadRequest, "error.tmpl", gin.H{"error": err.Error()})
+			return
+		}
+		library = append(library, book)
+		c.Redirect(http.StatusSeeOther, "/")
+	})
+
+	router.Run(":8080")
+
+}
+
+type Book struct {
+	Title  string
+	Author string
+	ISBN   string
+}
+
+func displayBook(b Book) {
+	fmt.Printf("Title: %s\nAuthor: %s\nISBN: %s\n", b.Title, b.Author, b.ISBN)
+}
+
+func validateISBN(isbn string) error {
+	if len(isbn) != 13 {
+		return errors.New("ISBN must be 13 characters long")
+	}
+	return nil
+}
+
+func createBook() (Book, error) {
+	var title, author, isbn string
+
+	fmt.Print("Enter Book Title: ")
+	fmt.Scanln(&title)
+
+	fmt.Print("Enter Author: ")
+	fmt.Scanln(&author)
+
+	fmt.Print("Enter ISBN: ")
+	fmt.Scanln(&isbn)
+
+	err := validateISBN(isbn)
+	if err != nil {
+		return Book{}, err
+	}
+
+	return Book{Title: title, Author: author, ISBN: isbn}, nil
+}
